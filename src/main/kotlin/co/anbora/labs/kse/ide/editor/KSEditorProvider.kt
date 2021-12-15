b807f5c8d2f9cecdb9537a84839f9e2e933ae0ff
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2021-12-15 00:37:16 -0500

    Added password dialog.

--- src/main/kotlin/co/anbora/labs/kse/ide/editor/KSEditorProvider.kt
+++ src/main/kotlin/co/anbora/labs/kse/ide/editor/KSEditorProvider.kt
@@ -1,15 +1,20 @@
 package co.anbora.labs.kse.ide.editor
 
+import co.anbora.labs.kse.ide.gui.KeyStorePasswordDialog
 import co.anbora.labs.kse.ide.gui.swing.TableEditorSwing
 import co.anbora.labs.kse.lang.core.KSFile.acceptKSFile
 import co.anbora.labs.kse.lang.KSLanguage.EDITOR_TYPE_ID
 import com.intellij.openapi.fileEditor.AsyncFileEditorProvider
 import com.intellij.openapi.fileEditor.FileEditor
 import com.intellij.openapi.fileEditor.FileEditorPolicy
+import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
-class KSEditorProvider: AsyncFileEditorProvider {
+class KSEditorProvider: AsyncFileEditorProvider, DumbAware {
+
+    private var ksPassword: String = ""
+
     override fun accept(project: Project, file: VirtualFile): Boolean = acceptKSFile(project, file)
 
     override fun createEditor(project: Project, file: VirtualFile): FileEditor =
@@ -20,6 +25,10 @@ class KSEditorProvider: AsyncFileEditorProvider {
     override fun getPolicy(): FileEditorPolicy = FileEditorPolicy.HIDE_DEFAULT_EDITOR
 
     override fun createEditorAsync(project: Project, file: VirtualFile): AsyncFileEditorProvider.Builder {
+        val passwordDialog = KeyStorePasswordDialog()
+        if (passwordDialog.showAndGet()) {
+            ksPassword = passwordDialog.getPassword()
+        }
         return object : AsyncFileEditorProvider.Builder() {
             override fun build(): FileEditor = TableEditorSwing()
         }
