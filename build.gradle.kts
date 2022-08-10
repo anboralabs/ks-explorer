90b0a76e82d90a83ad96bd044959e9e16b8a5bee
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2022-08-09 22:08:10 -0500

    Added support for pfx files.

---
+++ build.gradle.kts
@@ -0,0 +1,60 @@
+plugins {
+    id("java")
+    kotlin("jvm") version "1.6.21"
+    id("org.jetbrains.intellij") version "1.8.0"
+}
+
+group = "co.anbora.labs"
+version = "1.2.1"
+
+repositories {
+    mavenCentral()
+}
+
+configurations {
+    all {
+        // Allows using project dependencies instead of IDE dependencies during compilation and test running
+        resolutionStrategy.sortArtifacts(ResolutionStrategy.SortOrder.DEPENDENCY_FIRST)
+    }
+}
+
+dependencies {
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.68")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+}
+
+apply {
+    plugin("kotlin")
+    plugin("org.jetbrains.intellij")
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
+intellij {
+    version.set("2022.2")
+    type.set("IC") // Target IDE Platform
+    plugins.set(listOf())
+}
+
+tasks {
+    // Set the JVM compatibility versions
+    withType<JavaCompile> {
+        sourceCompatibility = "11"
+        targetCompatibility = "11"
+    }
+    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+
+    patchPluginXml {
+        sinceBuild.set("203")
+        untilBuild.set("222.*")
+        changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
+        pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
