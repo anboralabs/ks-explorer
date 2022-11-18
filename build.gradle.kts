1829bedf60545005a89dec4382db1c1fa78e82e4
Darwin Esteban Algarin Sarmiento <darwin.algarin@rappi.com>
2022-11-17 21:00:08 -0500

    upgrade sdk version.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.7.10"
-    id("org.jetbrains.intellij") version "1.9.0"
+    id("org.jetbrains.intellij") version "1.10.0"
 }
 
 group = "co.anbora.labs"
-version = "1.2.3"
+version = "1.2.4"
 
 repositories {
     mavenCentral()
