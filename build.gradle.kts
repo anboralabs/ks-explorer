5e23a19e13cdf3466ff95ed915e6476b9d26af13
Darwin Esteban Algarin Sarmiento <darwin.algarin@rappi.com>
2023-01-20 17:01:14 -0500

    upgrade sdk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ tasks {
     }
 
     patchPluginXml {
-        sinceBuild.set("223")
+        sinceBuild.set("231")
         untilBuild.set("231.*")
         changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
         pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
