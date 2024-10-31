1b8fa93f208f9b8fecb75527eb4acb540c12bbf8
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2024-10-31 01:05:12 +0000

    Change compatibility for paid plugins.

--- src/main/java/co/anbora/labs/kse/license/CheckLicense.java
+++ src/main/java/co/anbora/labs/kse/license/CheckLicense.java
@@ -1,10 +1,10 @@
 package co.anbora.labs.kse.license;
 
 import com.intellij.openapi.actionSystem.*;
+import com.intellij.openapi.actionSystem.ex.ActionUtil;
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.application.ModalityState;
 import com.intellij.ui.LicensingFacade;
-import java.awt.event.InputEvent;
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 import java.security.Signature;
@@ -145,7 +145,7 @@ public class CheckLicense {
       registerAction = actionManager.getAction("Register");
     }
     if (registerAction != null) {
-      registerAction.actionPerformed(AnActionEvent.createEvent(
+      ActionUtil.performActionDumbAwareWithCallbacks(registerAction, AnActionEvent.createEvent(
           asDataContext(productCode, message), new Presentation(), "",
           ActionUiKind.NONE, null));
     }
